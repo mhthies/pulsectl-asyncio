397e1e17f5feb7539ad8bb401cfe61ebed155db6
Michael Thies <mail@mhthies.de>
2021-09-11 17:10:51 +0200

    Fix pulsectl version requirement: 21.9.2 has not been released

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=['pulsectl_asyncio'],
     python_requires='~=3.6',
     install_requires=[
-        'pulsectl>=20.5.1,<=21.9.2',
+        'pulsectl>=20.5.1,<=21.9.1',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
